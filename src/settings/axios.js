98ed6a4d30d25dab8b493dd2b9223eb648e86e88
kyanokashi <kyanokashi2@gmail.com>
2022-03-15 13:24:51 +0200

    put server in env variable

--- src/settings/axios.js
+++ src/settings/axios.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 /** https://cryptix-backend.herokuapp.com */
 /** https://server.encore.fans */
 /** https://server.verify.encore.fans */
-const instance = axios.create({baseURL: 'https://server.verify.encore.fans'});
+const instance = axios.create({baseURL: process.env.REACT_APP_SERVER_URL});
 export default instance
